630f992381308593a8f00d1f60e29adc7517d330
Imran Khan Rafa <emailofimran1992@gmail.com>
2025-04-18 23:39:48 +0600

    data load

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,34 +1,37 @@
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ApiGatewayController } from './api-gateway.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { HrModule } from 'apps/hr/src/hr.module';
-import { Upload } from '../../../scalars/upload.scalar';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { ApolloDriverConfig, ApolloDriver } from "@nestjs/apollo";
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { ApiGatewayController } from "./api-gateway.controller";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { HrModule } from "apps/hr/src/hr.module";
+import { Upload } from "../../../scalars/upload.scalar";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
   imports: [
     Upload,
     ClientsModule.register([
       {
-        name: 'PAGE_BUILDER_SERVICE',
+        name: "PAGE_BUILDER_SERVICE",
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://user:password@rabbitmq:5673'],
-          queue: 'page_builder_queue',
+          urls: ["amqp://user:password@rabbitmq:5673"],
+          queue: "page_builder_queue",
           queueOptions: {
             durable: false,
+          },
+          socketOptions: {
+            frameMax: 131072, // 👈 set frameMax to a safe value (>= 8192)
           },
         },
       },
     ]),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql',
+      autoSchemaFile: "schema.gql",
       playground: true,
       //sortSchema: true,
       //typePaths: ['./**/*.graphql'],
@@ -42,17 +45,17 @@ import { join } from 'path';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
+        secret: configService.get("JWT_SECRET"),
         signOptions: {
           expiresIn: 3600,
         },
       }),
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'uploads'),
-      serveRoot: '/uploads',
+      rootPath: join(process.cwd(), "uploads"),
+      serveRoot: "/uploads",
       serveStaticOptions: {
-        extensions: ['jpg', 'jpeg', 'png', 'gif'],
+        extensions: ["jpg", "jpeg", "png", "gif"],
         index: false,
       },
     }),
